eedaf6ba1d78a81f0fdf77b7d9c3a8bef86c56a1
Congye Wang <congye.wang@outlook.com>
2024-05-02 15:22:51 +0100

    Add thin

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,17 @@
 
 int main()
 {
-    const int dim = 3;
-    const int n = 5;
-    arma::mat::fixed<n, dim> x(arma::fill::randn);
+    const int dim = 4;
+    const int num = 6;
+    arma::mat::fixed<num, dim> x(arma::fill::randn);
     arma::mat sx = -x;
     arma::vec::fixed<dim> x_map(arma::fill::randn);
     arma::mat::fixed<dim, dim> linv(arma::fill::eye);
     int s = 3;
     float beta = 0.5;
 
-    arma::mat::fixed<n, n> res_kmat_kgm;
-    arma::mat::fixed<n, n> res_kmat_imq;
+    arma::mat::fixed<num, num> res_kmat_kgm;
+    arma::mat::fixed<num, num> res_kmat_imq;
 
     linv = make_precon(x, sx, "id");
 
@@ -24,10 +24,10 @@ int main()
     auto stein_kernel_imq = make_imq(x, sx, "id");
     auto stein_kernel_centkgm = make_centkgm(x, sx, "id");
 
-    arma::mat::fixed<n, dim> a(arma::fill::randn);
-    arma::mat::fixed<n, dim> b(arma::fill::randn);
-    arma::mat::fixed<n, dim> sa(arma::fill::randn);
-    arma::mat::fixed<n, dim> sb(arma::fill::randn);
+    arma::mat::fixed<num, dim> a(arma::fill::randn);
+    arma::mat::fixed<num, dim> b(arma::fill::randn);
+    arma::mat::fixed<num, dim> sa(arma::fill::randn);
+    arma::mat::fixed<num, dim> sb(arma::fill::randn);
 
     arma::vec res_vec_centkgm;
     arma::vec res_vec_imq;
@@ -44,10 +44,10 @@ int main()
     arma::vec res_vfps_imq;
 
     res_vfps_centkgm = vfps(x_new, sx_new, x, sx, x_map, 1, vectorised_stein_kernel_centkgm);
-    res_vfps_centkgm.print("res_vfps_centkgm:");
-
     res_vfps_imq = vfps(x_new, sx_new, x, sx, 1, vectorised_stein_kernel_imq);
-    res_vfps_imq.print("res_vfps_imq:");
+
+    arma::uvec idx = thin(x, sx, 3);
+    idx.print("idx: ");
 
     return 0;
 }
