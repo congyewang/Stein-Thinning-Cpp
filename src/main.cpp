20e1dee7ce4e417b85699c4801e617b632534b4a
Congye Wang <congye.wang@outlook.com>
2024-05-02 23:53:54 +0100

    Add demo data, and add m param.

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,32 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+    if (argc != 4)
     {
-        std::cerr << "Usage: " << argv[0] << " <path_to_smp.csv> <path_to_scr.csv>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << "<int number_of_points> <str path_to_smp.csv> <str path_to_scr.csv>" << std::endl;
         return 1;
     }
-
+    int m;
     arma::mat smp;
     arma::mat scr;
 
-    bool status_smp = smp.load(argv[1]);
+    m = std::atoi(argv[1]);
+
+    bool status_smp = smp.load(argv[2]);
     if (!status_smp)
     {
         std::cerr << "Failed to load " << argv[1] << std::endl;
         return 1;
     }
 
-    bool status_scr = scr.load(argv[2]);
+    bool status_scr = scr.load(argv[3]);
     if (!status_scr)
     {
         std::cerr << "Failed to load " << argv[2] << std::endl;
         return 1;
     }
 
-    arma::uvec idx = thin(smp, scr, 3);
+    arma::uvec idx = thin(smp, scr, m);
     idx.print("idx: ");
 
     return 0;
