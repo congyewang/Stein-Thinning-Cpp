a6d2106a35ac7dc817ea4ed4febde3e59beece39
Congye Wang <congye.wang@outlook.com>
2024-08-24 22:11:24 +0100

    Switch to GoogleTest

--- test/test.cpp
+++ test/test.cpp
@@ -1,44 +1,42 @@
+#include <gtest/gtest.h>
 #include <armadillo>
-#include <cassert>
 #include "kernel/kernel.h"
 #include "kernel/thinning.h"
 
-int main(void)
+// Defining Test Cases
+TEST(ThinningTest, ThinningCorrectness)
 {
 
     int m = 40;
     arma::mat smp;
     arma::mat scr;
 
+    // Load smp.csv
     bool status_smp = smp.load("../../demo/sample_chains/gmm/smp.csv");
-    if (!status_smp)
-    {
-        std::cerr << "Failed to load smp.csv" << std::endl;
-        return 1;
-    }
+    ASSERT_TRUE(status_smp) << "Failed to load smp.csv";
 
+    // Load scr.csv
     bool status_scr = scr.load("../../demo/sample_chains/gmm/scr.csv");
-    if (!status_scr)
-    {
-        std::cerr << "Failed to load scr.csv" << std::endl;
-        return 1;
-    }
+    ASSERT_TRUE(status_scr) << "Failed to load scr.csv";
 
+    // Thinning
     arma::uvec idx = thin(smp, scr, m);
 
+    // Define gold_standard
     arma::uvec gold_standard = {
-         68, 322, 268, 234, 161, 292, 229, 275,
+        68, 322, 268, 234, 161, 292, 229, 275,
         259, 131, 400, 486, 207, 120, 443, 430,
-        376, 411,  98, 293, 111, 372, 285, 427,
-        406, 246, 148, 260, 296, 208,  79, 430,
-        369, 363, 462, 393, 321, 460, 373, 114
-        };
+        376, 411, 98, 293, 111, 372, 285, 427,
+        406, 246, 148, 260, 296, 208, 79, 430,
+        369, 363, 462, 393, 321, 460, 373, 114};
 
-    if (arma::all(idx == gold_standard)) {
-        std::cout << "Passed Test." << std::endl;
-    } else {
-        throw std::runtime_error("Thinning Index and Gold Standard are Not Identical!");
-    }
+    // Valid if idx and gold_standard are identical
+    EXPECT_TRUE(arma::all(idx == gold_standard)) << "Thinning Index and Gold Standard are Not Identical!";
+}
 
-    return 0;
+// Main Function
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
